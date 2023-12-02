8b51311468c0eed938ff538b107f2615397827f6
m0000amir <mukhtarov.amir.a@gmail.com>
2023-12-03 00:28:05 +0300

    refactored main page

--- src/services/DealerService.ts
+++ src/services/DealerService.ts
@@ -1,7 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '@src/services/common/consts/ApiConsts.ts';
 import { getBackendURL } from '@src/services/common/utils/apiUtils.ts';
-import { DealersResponse } from '@src/services/models.ts';
+import {
+  DealerProductsByIdRequest,
+  DealerProductsByIdResponse,
+  DealersResponse,
+} from '@src/services/models.ts';
 
 const ROOT_DEALER_PRODUCTS_URL = 'dealer';
 const ROOT_DEALERS_URL = 'dealers';
@@ -18,14 +22,15 @@ export const dealerApi = createApi({
   }),
   tagTypes: ['dealerProducts'],
   endpoints: (build) => ({
-    // TODO: fixed type (remove any)
-    getDealerProductsById: build.query<any, any>({
+    getDealerProductsById: build.query<
+      DealerProductsByIdResponse,
+      DealerProductsByIdRequest
+    >({
       query: ({ id, size, page }) => ({
         url: `${DEALER_ENDPOINTS.getDealerProductsById}/${id}?size=${size}&page=${page}`,
         method: 'GET',
       }),
     }),
-    // TODO: fixed type (remove any)
     getDealers: build.query<DealersResponse, void>({
       query: () => ({
         url: `${DEALER_ENDPOINTS.getDealers}`,
--- src/services/common/consts/ApiConsts.ts
+++ src/services/common/consts/ApiConsts.ts
@@ -1,1 +1,3 @@
 export const BASE_URL = 'http://localhost:8000';
+
+export const POINT_RETURN_API_URL = `${BASE_URL}/api/v1`;
