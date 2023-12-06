e6802f7308444d0e41869c25de286c084a180d48
m0000amir <mukhtarov.amir.a@gmail.com>
2023-12-06 23:55:37 +0300

    added point return url

--- src/services/common/consts/ApiConsts.ts
+++ src/services/common/consts/ApiConsts.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:8000';
+// export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = 'https://point-return.sytes.net';
 
 export const POINT_RETURN_API_URL = `${BASE_URL}/api/v1`;
 
