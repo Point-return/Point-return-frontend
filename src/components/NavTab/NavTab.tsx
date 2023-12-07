df87390dd0cd9117e98b69b4fe525e8252d260ba
m0000amir <mukhtarov.amir.a@gmail.com>
2023-12-07 22:42:23 +0300

    fixe main page

--- src/components/NavTab/NavTab.tsx
+++ src/components/NavTab/NavTab.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './NavTab.scss';
 import { Card, Button } from '@gravity-ui/uikit';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function NavTab() {
-  const location = useLocation();
-
   return (
     <Card className="navtab" view="clear">
       <Link to="/" className="navtab__link">
-        <Button selected={location.pathname === '/' && true}>Главная</Button>
+        <Button>Главная</Button>
       </Link>
     </Card>
   );
